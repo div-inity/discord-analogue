f968f19594ad9276b4291d8db38498446cf7a4ff
anna <veselaya.devka@ya.ru>
2025-10-20 13:20:59 +0300

    header есть

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,30 @@
   <!-- <button @click="toggleTheme" class="theme-switcher">
     {{ isDark ? '☀️' : '🌙' }} {{ currentTheme }}
   </button> -->
+  <header>
+    <div class="logo">Discord Analogue</div>
+    <div class="header-actions">
+      <button class="header-action minus">
+        <svg width="26" height="24" viewBox="0 0 26 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <rect width="26" height="24" fill="#202225" />
+          <path d="M6.5 12L19.5 12" stroke="white" stroke-linecap="round" stroke-linejoin="round" />
+        </svg>
+      </button>
+      <button class="header-action square">
+        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <rect width="24" height="24" fill="#202225" />
+          <path d="M14.4 6H9.6H6V9.6V14.4V18H9.6H14.4H18V14.4V9.6V6H14.4Z" stroke="white" stroke-linecap="round"
+            stroke-linejoin="round" />
+        </svg>
+      </button>
+      <button class="header-action cross">
+        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <rect width="24" height="24" fill="#202225" />
+          <path d="M6 6L18 18M18 6L6 18" stroke="white" stroke-linecap="round" stroke-linejoin="round" />
+        </svg>
+      </button>
+    </div>
+  </header>
   <router-view></router-view>
 </template>
 
@@ -31,6 +55,46 @@ onMounted(() => {
 
 html {
   background-image: none;
+}
+
+header {
+  width: 100%;
+  height: 24px;
+  background-color: var(--system-back-color5);
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  flex-wrap: wrap;
+  align-content: space-between;
+  padding-left: 8px;
+
+
+  .logo {
+    font-family: 'inter-600';
+    font-size: 12px;
+    line-height: 133%;
+    color: #fff;
+  }
+
+  .header-actions {
+    display: flex;
+    height: inherit;
+
+    .header-action {
+
+      &:hover {
+        rect {
+          fill: #4d4d4d;
+        }
+      }
+
+      /* .cross:hover { не работает
+        rect {
+          fill: red !important;
+        }
+      } */
+    }
+  }
 }
 
 .theme-switcher {
--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,11 @@
 import { createRouter, createWebHashHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
+import MainView from "../views/MainView.vue";
 
 const routes = [
   {
     path: "/",
-    name: "home",
-    component: HomeView,
+    name: "main",
+    component: MainView,
   },
   {
     path: "/about",
